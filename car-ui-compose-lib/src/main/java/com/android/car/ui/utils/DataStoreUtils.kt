c51e4eaac617b3abd0d690c30427f4e279d25284
Jawahar Venugopal <jawahar456@gmail.com>
2025-07-20 00:48:31 +0530

    Fix build issue

--- car-ui-compose-lib/src/main/java/com/android/car/ui/utils/DataStoreUtils.kt
+++
@@ -1,6 +0,0 @@
-package com.android.car.ui.utils
-
-import android.content.Context
-import androidx.datastore.preferences.preferencesDataStore
-
-val Context.dataStore by preferencesDataStore(name = "car_ui_lib_prefs")
